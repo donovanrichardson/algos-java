4289054148a30f35ce84042fbba71b3df15f8a97
donovanrichardson <donovanspqa@gmail.com>
2021-01-26 21:24:44 -0500

    it ran

--- src/com/msa/Solution.java
+++ src/com/msa/Solution.java
@@ -1,7 +1,30 @@
 package com.msa;
 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+    void shift(int curIdx, int endIdx, int[] nums1, int newItem){
+        while(endIdx >= curIdx){
+            nums1[endIdx+1] = nums1[endIdx];
+        }
+        nums1[curIdx] = newItem;
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a = 0;
+        int b = m - 1;
+//        System.out.println("here ye hear ye");
+        for(int n2 : nums2){
+            System.out.println(n2);
+            if(b >= a){
+//                System.out.println(b);
+//                System.out.println(a);
+                if(n2 < nums1[a]){
+                    System.out.println("not a blank string");
+                     this.shift(a,b,nums1,n2);
+                }
+            }
+            nums1[a] = n2;
+            a++;
+        }
     }
 }
